16e084d8972460bccac6a3776e31f9edf661450a
gov <govi218mu@gmail.com>
2019-03-28 01:08:39 -0400

    declarative notary

--- notarize.pl
+++ notarize.pl
@@ -1,10 +1,21 @@
-has_hash(Document, HexAtom):-
+get_hash(Document, HexAtom):-
     sha_hash(Document, Hash, []),
     hash_atom(Hash, HexAtom).
 
-proof_for(Document, Proof):-
-    has_hash(Document, Hash),
-    compare(=, Hash, Proof).
+store_hash(Hash):-
+    open('proofs.txt',write,Stream), 
+    atom_concat(Hash, ".", Store),    
+    write(Stream, Store),  nl(Stream), 
+    close(Stream).
 
-notarized(Document, X):-
-    proof_for(Document, X).
+proof_for(Document):-
+    get_hash(Document, Hash),
+    open('proofs.txt',read,Str),
+    read(Str, StoredHash), 
+    compare(=, Hash, StoredHash),
+    close(Str),
+    write(StoredHash),  nl.
+
+notarize(Document):-
+    get_hash(Document, Hash),
+    store_hash(Hash).
