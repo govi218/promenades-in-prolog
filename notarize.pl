912808199f55a1a89f2702bed8eb90e403b74664
gov <govi218mu@gmail.com>
2019-03-28 16:01:40 -0400

    comments

--- notarize.pl
+++ notarize.pl
@@ -1,13 +1,17 @@
+%% stores the SHA256 hash of Document in HexAtom
 get_hash(Document, HexAtom):-
     sha_hash(Document, Hash, []),
     hash_atom(Hash, HexAtom).
 
+%% writes the hash into the proof file as an atom 
+% (needs to handle hashes that start with uppercase or numbers)
 store_hash(Hash):-
     open('proofs.txt',write,Stream), 
     atom_concat(Hash, ".", Store),    
     write(Stream, Store),  nl(Stream), 
     close(Stream).
 
+%% checks if there's a stored proof for the document
 proof_for(Document):-
     get_hash(Document, Hash),
     open('proofs.txt',read,Str),
@@ -16,6 +20,7 @@ proof_for(Document):-
     close(Str),
     write(StoredHash),  nl.
 
+%% stores a proof for a document
 notarize(Document):-
     get_hash(Document, Hash),
     store_hash(Hash).
